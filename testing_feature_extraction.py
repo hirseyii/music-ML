c2a83e47235167bb65f75b3cd8604fb1fbe60dc7
Naim Sen <naim.k.sen@gmail.com>
2018-06-18 13:14:47 +0100

    Testing chromagrams with RHCP

---
+++ testing_feature_extraction.py
@@ -0,0 +1,26 @@
+"""
+Naim Sen
+-Jun 18-
+
+"""
+# A script to test simple feature extraction using librosa
+# Test using audio included in the sample folder (Red Hot Chilli Peppers albums)
+
+import numpy as np
+import librosa.display
+import librosa as lb
+import matplotlib.pyplot as plt
+
+#select a single audio track
+filename = 'sample/03 Scar Tissue.mp3'
+
+#call lb load
+track, sr = lb.load(filename)
+#compute chromagram
+chromagram_cqt = lb.feature.chroma_cqt(y=track, sr=sr)
+# display cqt
+plt.figure(figsize=(10, 4))
+lb.display.specshow(chromagram_cqt)
+plt.colorbar()
+plt.title("cqt")
+plt.show()
