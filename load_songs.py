db861c6c642536c608c71c6f16fb0d2d42a398b7
Naim Sen <naim.k.sen@gmail.com>
2018-07-05 13:59:53 +0100

    bugfix - savefile name

--- load_songs.py
+++ load_songs.py
@@ -277,10 +277,12 @@ if __name__ == "__main__":
         #create song database, songdb:
         songname_tmp=[]
         songpath_tmp=[]
-        path='/raid/scratch/sen/music-ML/sample/'
+        load_path_root='/raid/scratch/sen/music-ML/'
+        load_filename='sample'
+        path=load_path_root+load_filename
         #path=sys.argv[1] #the only command line input is the path to the folder of music
         print(path)
-        savefile=str(path)+'_data' #it's saved with the same folder name but with _data.pkl on the end.
+        savefile=load_filename+'_data' #it's saved with the same folder name but with _data.pkl on the end.
         #now load song data in
         for song in os.listdir(path):
             #print (song)
