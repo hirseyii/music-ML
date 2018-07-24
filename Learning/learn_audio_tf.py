83ece4ac6098cf44aaf6748ae70fc65d1ec16d16
Naim Sen <naim.k.sen@gmail.com>
2018-07-24 13:07:45 +0100

    First keras model try

--- Learning/learn_audio_tf.py
+++ Learning/learn_audio_tf.py
@@ -13,7 +13,10 @@ import matplotlib
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 from Learning.learn_audio_sklearn import prepare_data
+
 
 
 # =================== main ======================
@@ -25,22 +28,51 @@ if __name__ == '__main__':
     # adjust for your display if necessary
     matplotlib.rcParams['figure.figsize'] = [18, 9]
 
-    # load in previously sved data
+    # load in previously saved data we will use similar calling idioms
+    # to the sklearn version.
     path = sys.argv[1]
+    train_perc = float(sys.argv[2])
     all_data = glob.glob(path + '/*_data.pkl')
     # first we want to get all our data loaded in
     all_features, all_labels, feature_names = prepare_data(all_data, path)
+    n_labels = len(np.unique(all_labels))
+    n_features = len(feature_names)
+    n_samples = len(all_labels)
 
     # we need to turn our string vector of class labels into ints
-    # so we may as well binarize too
-    all_labels = pd.get_dummies(all_labels)
+    # use sklearn preprocessing
+    label_encoder = preprocessing.LabelEncoder()
+    all_labels = label_encoder.fit_transform(all_labels)
+    # now transform to one-hot (could use tf if we want but it makes a tensor)
+    ## all_labels = tf.one_hot(all_labels, n_labels)
+    all_labels = keras.utils.to_categorical(all_labels)
     all_features = np.array(all_features)
-    #  make tf dataset from labels and features
-    assert all_features.shape[0] == all_labels.shape[0]
-    dataset = tf.data.Dataset.from_tensor_slices((all_features, all_labels))
-    dataset = dataset.shuffle(len(all_labels))
-    iterator = dataset.make_one_shot_iterator()
-    el = iterator.get_next()
-    with tf.Session() as sess:
-        print(sess.run(el))
-    
+
+    # train test split
+    x_train, x_test, y_train, y_test = train_test_split(all_features,
+                                                        all_labels,
+                                                        train_size = train_perc,
+                                                        random_state = 0,
+                                                        stratify = all_labels
+                                                        )
+    # scale data
+    scaler = preprocessing.StandardScaler()
+    scaler.fit(x_train)
+    x_train = scaler.transform(x_train)
+    x_test = scaler.transform(x_test)
+
+    # Construct keras sequential model
+    model = keras.Sequential()
+    # input layer
+    model.add(keras.layers.Dense(n_features, activation='sigmoid'))
+    # hidden layers
+    model.add(keras.layers.Dense((n_features+n_classes)/2, activation='sigmoid'))
+    # output layer
+    model.add(keras.layers.Dense(n_classes, activation='softmax'))
+    # compile
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='Adam',
+                  metrics=['accuracy'])
+
+    # fit the model
+    history = model.fit(x_train, y_train, epochs=50, validation_split=0.10)
